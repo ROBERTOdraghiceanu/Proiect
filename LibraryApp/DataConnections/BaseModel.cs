a528f4e360ee4b7320372eca6a8f1317819c94e0
SerbanCornelD <62016486+SerbanCornelD@users.noreply.github.com>
2020-04-05 15:22:46 +0300

    Add files via upload

---
+++ LibraryApp/DataConnections/BaseModel.cs
@@ -0,0 +1,30 @@
+﻿using ClassImplementation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataConnections
+{
+    public class BaseModel<T> : IModel<T>
+    {
+        public T Add(T itemToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T itemToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T itemToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
