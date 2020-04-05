0c338f438c8b370442ad33019deab7731a00dc6b
SerbanCornelD <62016486+SerbanCornelD@users.noreply.github.com>
2020-04-05 15:23:45 +0300

    Add files via upload

---
+++ LibraryApp/DataConnections/TransactionManagerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ClassImplementation.Logic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataConnections
+{
+    public class TransactionManagerDbContext : DbContext
+    {
+        public TransactionManagerDbContext(DbContextOptions<TransactionManagerDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+    }
+}
